b18fd67a7bf1ea8a489c0b782b9bff1aea3eec86
Riceman2000 <alex4222000@gmail.com>
2024-08-24 01:21:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,107 @@
+# d2c-rs
+
+Update Cloudflare DNS 'A' records with your public IP.
+
+Created to be a drop in replacement for this tool: https://github.com/ddries/d2c-rs
+
+---
+
+d2c-rs (Dynamic DNS Cloudflare) is a very simple program to automatically update the IP address of A DNS records from Cloudflare using your current public IP. This tool is designed to be run regularly e.g. on a [cron](https://en.wikipedia.org/wiki/Cron) job.
+
+### Configure
+
+d2c-rs is configured using TOML files located in `/etc/d2c/`. The first time you run d2c-rs from the command-line, it will create the config directory for you. You will then need to manually create one or more TOML configuration files.
+
+The script processes all files in `/etc/d2c/` directory that end with `.toml`, e.g. `/etc/d2c/d2c.toml`, `/etc/d2c/zone1.toml` or `/etc/d2c/zone2.toml`.
+
+Syntax:
+
+```toml
+[api]
+zone-id = "aaa" # your DNS zone ID
+api-key = "bbb" # your API key with DNS records permissions
+
+[[dns]]
+name = "dns1.example.com" # DNS name
+proxy = true              # Proxied by Cloudflare?
+
+[[dns]]
+name = "dns2.example.com"
+proxy = false
+```
+
+When d2c-rs is run, it will process each `*.toml` TOML file in the `/etc/d2c/` directory, updating the records configured in each with the current public IP of the machine. The A records must be created from the Cloudflare dashboard first; then d2c-rs will be able to update them with the server's public IP. 
+
+### Usage
+```sh
+d2c-rs --help
+```
+Prints the following information:
+
+```
+d2c (Dynamic Dns Cloudflare): Update the Cloudflare DNS A records for your dynamic IP.
+
+Usage: d2c-rs
+
+`d2c` UPDATES existing records. Please, create them in Cloudflare Dashboard before running this script.
+
+The configuration is done in `/etc/d2c/*.toml` files in TOML format.
+Configuration file structure:
+
+[api]
+zone-id = "<zone id>"
+api-key = "<api key>"
+
+[[dns]]
+name = "test.example.com"
+proxy = false
+
+[[dns]]
+name = "test2.example.com"
+proxy = true
+```
+
+#### Installing d2c-rs
+
+Install d2c-rs using Cargo:
+
+```sh
+cargo install d2c-rs
+```
+
+Then, run d2c-rs from command-line for the first time to create the configuration directory:
+
+```sh
+d2c-rs
+```
+
+Create configuration file(s) with your zone id, API key and the desired DNS records. Only one pair of `zone-id` and `api-key` in each file. If you have any other zones or another Cloudflare account you want to configure it is possible by creating additional TOML files.
+
+```sh
+sudo nano /etc/d2c/d2c.toml
+```
+
+```toml
+[api]
+zone-id = "aaa"
+api-key = "bbb"
+
+[[dns]]
+name = "test.example.com"
+proxy = false
+
+[[dns]]
+name = "test2.example.com"
+proxy = true
+```
+
+Finally, you can run d2c-rs to update the DNS records specified in your configuration files:
+
+```sh
+d2c-rs
+```
+or create a cronjob:
+
+```sh
+crontab -e # set cronjob to run d2c-rs periodically
+```
---
+++ src/main.rs
@@ -0,0 +1,142 @@
+use std::{env, path::PathBuf, process};
+
+use cloudflare::{
+    endpoints::dns,
+    framework::{self, async_api, auth},
+};
+use serde_derive::Deserialize;
+use tokio::fs;
+use tracing::{error, info};
+
+const CONFIG_DIR: &str = "/etc/d2c";
+const SAMPLE_CONFIG: &str = include_str!("./sample_config.toml");
+const USAGE: &str = include_str!("./usage.txt");
+
+#[derive(Debug, Deserialize)]
+struct ConfigFile {
+    #[serde(skip)]
+    file_name: String,
+    api: Api,
+    dns: Vec<Dns>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Api {
+    #[serde(alias = "zone-id")]
+    zone_id: String,
+
+    #[serde(alias = "api-key")]
+    api_key: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Dns {
+    name: String,
+    proxy: bool,
+}
+
+#[tokio::main]
+async fn main() {
+    // Send usage string
+    let args: Vec<String> = env::args().collect();
+    if args.iter().any(|arg| arg == "--help" || arg == "-h") {
+        println!("{USAGE}");
+        process::exit(0);
+    }
+
+    // Setup loging
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
+    // Validate config dir
+    let config_dir = PathBuf::from(CONFIG_DIR);
+    if !config_dir.exists() {
+        error!("Config directory at {CONFIG_DIR} does not exist, creating the directory");
+        match fs::create_dir(config_dir)
+            .await {
+            Ok(()) =>  info!("Directory created, you must populate it with at least one config file following this format:\n{SAMPLE_CONFIG}"),
+            Err(e) => error!("Failed to create config directory with: {e}"),
+        }
+        process::exit(1);
+    }
+    if !config_dir.is_dir() {
+        error!("Config dir at {CONFIG_DIR} is not a direcory");
+        process::exit(1);
+    }
+
+    // Resolve public IP
+    info!("Finding public IP");
+    let Some(public_ip) = public_ip::addr_v4().await else {
+        error!("Failed to resolve public IP");
+        process::exit(1);
+    };
+    info!("Public IP found as {public_ip}");
+
+    // Process config files
+    info!("Parsing config directory {CONFIG_DIR}");
+    let mut config_files = match fs::read_dir(CONFIG_DIR).await {
+        Ok(f) => f,
+        Err(e) => {
+            error!("Failed to read config directory with: {e}");
+            process::exit(1);
+        }
+    };
+    let mut configs = Vec::new();
+    while let Ok(Some(file)) = config_files.next_entry().await {
+        info!("Found file {file:?}");
+        let file_name = file
+            .file_name()
+            .into_string()
+            .expect("Malformed file name {file:#?}");
+        let content = fs::read_to_string(file.path())
+            .await
+            .expect("Failed to read file");
+
+        let mut config: ConfigFile = match toml::from_str(&content) {
+            Ok(c) => c,
+            Err(e) => {
+                error!("Failed to read file {file_name} with {e}");
+                continue;
+            }
+        };
+        config.file_name = file_name;
+        info!("File content: {config:#?}");
+        configs.push(config);
+    }
+    info!("Configurations collected from {CONFIG_DIR}");
+
+    // Update records
+    for config in configs {
+        info!("Processing file {}", config.file_name);
+        let credentials = auth::Credentials::UserAuthToken {
+            token: config.api.api_key,
+        };
+        let api_config = framework::HttpApiClientConfig::default();
+        let environment = framework::Environment::Production;
+        let client = match async_api::Client::new(credentials, api_config, environment) {
+            Ok(c) => c,
+            Err(e) => {
+                error!("Failed to form API client with {e}");
+                continue;
+            }
+        };
+
+        let dns_list_request = dns::ListDnsRecords {
+            zone_identifier: &config.api.zone_id,
+            params: dns::ListDnsRecordsParams::default(),
+        };
+        let dns_list = match client.request(&dns_list_request).await {
+            Ok(d) => d,
+            Err(e) => {
+                error!("API request to list existing records failed with {e}");
+                continue;
+            }
+        };
+        info!("Existing records:\n{dns_list:#?}");
+
+        for dns in config.dns {
+            info!("Processing record {}", dns.name);
+            info!("Record {} updated", dns.name);
+        }
+    }
+}
